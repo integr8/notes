60a892838066914ed7b751f91446ad025fcde928
Fábio Luciano <fabio@naoimporta.com>
2020-06-24 00:50:21 -0300

    feat(vue): :sparkles: Adiciona a tela de login

---
+++ src/vue/src/components/Login.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <v-app id="inspire">
+      <v-main>
+        <v-container class="fill-height" fluid>
+          <v-row align="center" justify="center">
+            <v-col cols="20" sm="8" md="4">
+              <v-card class="elevation-12">
+                <v-toolbar color="primary" dark flat>
+                  <v-toolbar-title>Login</v-toolbar-title>
+                  <v-spacer></v-spacer>
+                </v-toolbar>
+                <v-card-text>
+                  <v-form>
+                    <v-text-field
+                      label="Usuário"
+                      name="username"
+                      prepend-icon="mdi-account"
+                      type="text"
+                    ></v-text-field>
+
+                    <v-text-field
+                      id="password"
+                      label="Senha"
+                      name="password"
+                      prepend-icon="mdi-lock"
+                      type="password"
+                    ></v-text-field>
+                  </v-form>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="primary">Login</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-main>
+    </v-app>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Login",
+  data: () => ({}),
+});
+</script>
--- src/vue/src/main.ts
+++ src/vue/src/main.ts
@@ -1,16 +1,15 @@
 import Vue from "vue";
+import "./plugins/axios";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import Buefy from "buefy";
-import "buefy/dist/buefy.css";
-
-Vue.use(Buefy);
+import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
