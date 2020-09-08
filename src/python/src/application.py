42a41a606cab683f07c7892abab08ca33765680b
Fábio Luciano <fabio@naoimporta.com>
2020-09-08 01:20:22 -0300

    refactor(python): Adiciona procedimentos para execução da aplicação no Dockerfile

--- src/python/src/application.py
+++ src/python/src/application.py
@@ -1,10 +1,6 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask
-from flask_restful import Api
+from flask_restful import Api, Resource
 from flask_jwt import JWT
-
-application = Flask(__name__)
-application.secret_key = '53cr3t'
-api = Api(application)
-
-if __name__ == '__main__':
-    application.run(port=5000, debug=True)
+import newspaper
